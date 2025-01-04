63cb6430a0c1751a5a9bac5a6aca6f185787a31e
Ufgt_NOI <strelok1579@gmail.com>
2025-01-04 17:49:36 +0700

    Добавлены названия элементов и переделана логика

--- app/src/main/java/com/example/laba1/MainActivity.kt
+++ app/src/main/java/com/example/laba1/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.example.laba1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
+        checkButton.setOnClickListener {
+            val input = inputField.text.toString()
+
+            val symbol = input[0]
+            if (!symbol.isLowerCase() || !symbol.isLetter()) {
+                // Проверка, что символ является строчной латинской буквой
+                resultText.text = "Символ не является строчной латинской буквой"
+            } else if ("aeiou".contains(symbol)) {
+                // Проверка на гласные
+                resultText.text = "Это гласные буквы"
+            } else {
+                // Остальные буквы
+                resultText.text = "Возможно, это согласные буквы"
+            }
         }
     }
-
-
 }
